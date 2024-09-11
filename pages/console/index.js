d39562cb176dae9c65f49aa26509e922bfd328c6
luizfiorentino <lgfiorentino@gmail.com>
2024-09-11 17:13:06 +0200

    added base for first part of the app

---
+++ pages/console/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function index() {
+  const menu = [
+    { name: "Marguerita", price: 8 },
+    { name: "Pepperoni", price: 10 },
+    { name: "Hawaiian", price: 8 },
+    { name: "Marguerita", price: 10 },
+    { name: "Veggie", price: 9 },
+  ];
+
+  const cashInRegister = 100;
+  const orderQueue = [];
+
+  // Add utility function that takes a pizza object and adds it to the menu
+
+  return (
+    <div>
+      <h1>App Explorer</h1>
+    </div>
+  );
+}
